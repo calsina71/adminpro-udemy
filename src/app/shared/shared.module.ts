7a02515a8c1d35a7c02ffadac264106588e0504c
Carlos Alsina <calsina71@gmail.com>
2020-03-25 19:22:39 +0100

    Modificaciones de los móduclos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { Page404Component } from './page404/page404.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    Page404Component,
+    BreadcrumbsComponent
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    Page404Component,
+    BreadcrumbsComponent
+  ]
+})
+
+export class SharedModule { }
